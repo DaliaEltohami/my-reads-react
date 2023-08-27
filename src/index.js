dd583520fede6ad460ec04e11b3bcdee71ce9649
dalia eltohami <daliaeltohami1991@gmail.com>
2023-08-27 11:16:16 +0300

    y

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
